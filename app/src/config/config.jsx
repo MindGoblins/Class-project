c886e327167947dfd81994b639d59a3a71af3f8f
MindGoblins <adammiller6945@gmail.com>
2024-09-16 10:56:09 +0000

    bootstrap and firebase

---
+++ app/src/config/config.jsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD8M3quns52DaSXO8yDYQ9cT6dlS9z_HGw",
+    authDomain: "class-project-905c1.firebaseapp.com",
+    projectId: "class-project-905c1",
+    storageBucket: "class-project-905c1.appspot.com",
+    messagingSenderId: "739193461231",
+    appId: "1:739193461231:web:639d5f220e41fab443fe1a",
+    measurementId: "G-DHW23ETHP9"
+  };
